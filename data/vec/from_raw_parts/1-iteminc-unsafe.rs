e63b0ee991fb1294815bfaf6d4052ea8fa92981e
XU, Hui <china.xuhui@gmail.com>
2023-06-06 14:08:47 +0800

    Update 1-iteminc-unsafe.rs

--- data/vec/from_raw_parts/1-iteminc-unsafe.rs
+++ data/vec/from_raw_parts/1-iteminc-unsafe.rs
@@ -1,14 +1,21 @@
 //this case is from std: https://doc.rust-lang.org/std/vec/struct.Vec.html#method.into_raw_parts
-let v = vec![1, 2, 3];
-let mut v = mem::ManuallyDrop::new(v);
-let p = v.as_mut_ptr();
-let len = v.len();
-let cap = v.capacity();
+#![allow(unused)]
 
-unsafe {
-    for i in 0..len {
-        ptr::write(p.add(i), 4 + i);
+use std::ptr;
+use std::mem;
+
+fn main() {
+    let v = vec![1, 2, 3];
+    let mut v = mem::ManuallyDrop::new(v);
+    let p = v.as_mut_ptr();
+    let len = v.len();
+    let cap = v.capacity();
+
+    unsafe {
+        for i in 0..len {
+            ptr::write(p.add(i), 4 + i);
+        }
+        let rebuilt = Vec::from_raw_parts(p, len, cap);
+        assert_eq!(rebuilt, [4, 5, 6]);
     }
-    let rebuilt = Vec::from_raw_parts(p, len, cap);
-    assert_eq!(rebuilt, [4, 5, 6]);
 }
