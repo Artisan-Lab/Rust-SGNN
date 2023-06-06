c6260691a6192345259f23a9b084abc9a5240273
XU, Hui <china.xuhui@gmail.com>
2023-06-06 13:45:43 +0800

    Update and rename data/vec/from_raw_parts.rs to data/vec/from_raw_parts/1-iteminc-unsafe.rs

---
+++ data/vec/from_raw_parts/1-iteminc-unsafe.rs
@@ -0,0 +1,14 @@
+//this case is from std: https://doc.rust-lang.org/std/vec/struct.Vec.html#method.into_raw_parts
+let v = vec![1, 2, 3];
+let mut v = mem::ManuallyDrop::new(v);
+let p = v.as_mut_ptr();
+let len = v.len();
+let cap = v.capacity();
+
+unsafe {
+    for i in 0..len {
+        ptr::write(p.add(i), 4 + i);
+    }
+    let rebuilt = Vec::from_raw_parts(p, len, cap);
+    assert_eq!(rebuilt, [4, 5, 6]);
+}
