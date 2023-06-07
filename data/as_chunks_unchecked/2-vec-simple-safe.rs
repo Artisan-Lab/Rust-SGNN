e0aed22ae28b948ce897e022c742ed2874e6f9ff
XU, Hui <china.xuhui@gmail.com>
2023-06-07 11:23:00 +0800

    Update 2-vec-simple-safe.rs

--- data/as_chunks_unchecked/2-vec-simple-safe.rs
+++ data/as_chunks_unchecked/2-vec-simple-safe.rs
@@ -2,7 +2,7 @@
 #![feature(slice_as_chunks)]
 fn main() {
     let v = vec!['l', 'o', 'r', 'e', 'm'];
-    let (chunks, remainder) = v.as_chunks();
+    let (chunks, remainder) = v.as_chunks::<2>();
     assert_eq!(chunks, &[['l', 'o'], ['r', 'e']]);
     assert_eq!(remainder, &['m']);
 }
