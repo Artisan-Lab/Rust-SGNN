6adfecf5534aa8e16ef63b51c75a16f0b758c2ec
XU, Hui <china.xuhui@gmail.com>
2023-06-07 12:23:28 +0800

    Update 1-slice-simple-safe.rs

--- data/split_at_unchecked/1-slice-simple-safe.rs
+++ data/split_at_unchecked/1-slice-simple-safe.rs
@@ -3,7 +3,7 @@
 
 fn main() {
 let v = [1, 2, 3, 4, 5, 6];
-    let (left, right) =   unsafe {v.split_at(2)  };
+    let (left, right) = v.split_at(2);
     assert_eq!(left, [1, 2]);
     assert_eq!(right, [3, 4, 5, 6]);
 }
