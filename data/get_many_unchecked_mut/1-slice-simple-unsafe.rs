c42d31b99e1b8b4270c0f80d8bab15ec3dfb91c7
XU, Hui <china.xuhui@gmail.com>
2023-06-07 13:00:38 +0800

    Update 1-slice-simple-unsafe.rs

--- data/get_many_unchecked_mut/1-slice-simple-unsafe.rs
+++ data/get_many_unchecked_mut/1-slice-simple-unsafe.rs
@@ -1,5 +1,5 @@
 /*
-    This is a case from std
+    This is a slightly modified case from std
     Link: https://doc.rust-lang.org/std/vec/struct.Vec.html#method.get_many_unchecked_mut
     Purpose: Returns mutable references to many indices at once, without doing any checks.
     Replaceable? Yes
@@ -9,9 +9,9 @@
 #![feature(get_many_mut)]
 
 fn main() {
-let x = &mut [1, 2, 4];
-    let [a, b] = unsafe { x.get_many_unchecked_mut([0, 2]) };
-    *a *= 10;
-    *b *= 100;
-    assert_eq!(x, &[10, 2, 400]);
+    let x = &mut [1, 2, 3, 4];
+    let [a, b, d] = unsafe { x.get_many_unchecked_mut([0, 1, 3]) };
+    *a = 10;
+    *b = 100;
+    assert_eq!(x, &[10, 100, 3, 4]);
 }
