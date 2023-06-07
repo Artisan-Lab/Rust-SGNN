81ef33ca129cbf3ffdfc511d7b0684646caafb2b
XU, Hui <china.xuhui@gmail.com>
2023-06-07 11:23:12 +0800

    Update 1-slice-simple-safe.rs

--- data/as_chunks_unchecked/1-slice-simple-safe.rs
+++ data/as_chunks_unchecked/1-slice-simple-safe.rs
@@ -2,7 +2,7 @@
 #![feature(slice_as_chunks)]
 fn main() {
     let slice = ['l', 'o', 'r', 'e', 'm'];
-    let (chunks, remainder) = slice.as_chunks();
+    let (chunks, remainder) = slice.as_chunks::<2>();
     assert_eq!(chunks, &[['l', 'o'], ['r', 'e']]);
     assert_eq!(remainder, &['m']);
 }
