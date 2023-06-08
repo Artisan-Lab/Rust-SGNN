bdb756c2bf0628b25920d6f1ad6bf99669bf19f3
XU, Hui <china.xuhui@gmail.com>
2023-06-08 18:27:07 +0800

    Create 3-fromvec-safe.rs

---
+++ data/String::from_raw_parts/3-fromvec-safe.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+
+use std::mem;
+
+fn main() {
+    let v = vec![b'h',b'e',b'l',b'l',b'o'];
+    let mut s = String::new();
+    let l = v.len();
+    s.reserve(l);
+    for i in 0..l {
+        s.insert(i, v[i] as char);
+    }
+    assert_eq!(String::from("hello"), s);
+}
