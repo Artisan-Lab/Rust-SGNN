cb3cf7432cc4525d0a1e4c2fa4ded150a6029ceb
XU, Hui <china.xuhui@gmail.com>
2023-06-08 12:15:33 +0800

    Create 1-fromstring-unsafe.rs

---
+++ data/String::from_raw_parts/1-fromstring-unsafe.rs
@@ -0,0 +1,22 @@
+/*
+    This is a case from std
+    Link: https://doc.rust-lang.org/std/vec/struct.Vec.html#method.from_raw_parts 
+    Purpose: change the value of each Vec item
+    Replaceable? Yes
+*/
+
+#![allow(unused)]
+use std::mem;
+use std::ptr;
+
+fn main() {
+    let s = String::from("hello");
+    let mut s = mem::ManuallyDrop::new(s);
+
+    let p = s.as_mut_ptr();
+    let len = s.len();
+    let capacity = s.capacity();
+   
+    let s = unsafe { String::from_raw_parts(p, len, capacity) }; 
+    assert_eq!(String::from("hello"), s);
+}
