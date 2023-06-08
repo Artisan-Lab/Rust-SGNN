5f1e348c237a87709de1b8c9a3369b0cf7120ffb
XU, Hui <china.xuhui@gmail.com>
2023-06-08 19:44:54 +0800

    Update 3-fromvec-safe.rs

--- data/String::from_raw_parts/3-fromvec-safe.rs
+++ data/String::from_raw_parts/3-fromvec-safe.rs
@@ -6,7 +6,6 @@ fn main() {
     let v = vec![b'h',b'e',b'l',b'l',b'o'];
     let mut s = String::new();
     let l = v.len();
-    s.reserve(l);
     for i in 0..l {
         s.insert(i, v[i] as char);
     }
