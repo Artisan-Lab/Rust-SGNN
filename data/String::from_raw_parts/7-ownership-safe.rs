2896945b0b1672ca1a12ce57adfbcf7b16db2b59
XU, Hui <china.xuhui@gmail.com>
2023-06-09 17:11:55 +0800

    Update 7-ownership-safe.rs

--- data/String::from_raw_parts/7-ownership-safe.rs
+++ data/String::from_raw_parts/7-ownership-safe.rs
@@ -7,6 +7,7 @@ fn main() {
     let p = s.as_ptr();
     let l = s.len();
     USE(p);
+    USE(s);
     /*
     let slice = unsafe { std::slice::from_raw_parts(p, l) };
     assert_eq!(String::from("hello"), s);
