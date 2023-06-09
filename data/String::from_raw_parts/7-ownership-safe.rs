7571419200423786affe9546c0d0624a1539f075
XU, Hui <china.xuhui@gmail.com>
2023-06-09 17:09:53 +0800

    Create 7-ownership-safe.rs

---
+++ data/String::from_raw_parts/7-ownership-safe.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+#![feature(vec_into_raw_parts)]
+use std::mem;
+
+fn main() {
+    let s = String::from("hello");
+    let p = s.as_ptr();
+    let l = s.len();
+    let slice = unsafe { std::slice::from_raw_parts(p, l) };
+    assert_eq!(String::from("hello"), s);
+}
