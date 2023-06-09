28c5406248b032257fdfa41c9aadd301bc55a27a
XU, Hui <china.xuhui@gmail.com>
2023-06-09 17:11:08 +0800

    Update 7-ownership-unsafe.rs

--- data/String::from_raw_parts/7-ownership-unsafe.rs
+++ data/String::from_raw_parts/7-ownership-unsafe.rs
@@ -1,4 +1,3 @@
-
 #![allow(unused)]
 #![feature(vec_into_raw_parts)]
 use std::mem;
@@ -6,9 +5,12 @@ use std::mem;
 fn main() {
     let s = String::from("hello");
     let (p, l, c) = s.into_raw_parts();
+    USE(p);
+    /*
     let s1 = unsafe {
         let slice = std::slice::from_raw_parts(p, l);
         String::from_raw_parts(p, l, c)
     };
     assert_eq!(String::from("hello"), s1);
+    */
 }
