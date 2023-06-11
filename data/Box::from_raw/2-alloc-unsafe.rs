369cad79df8e5555622d7189b76d5cc102a6c4c6
XU, Hui <china.xuhui@gmail.com>
2023-06-11 13:31:58 +0800

    Create 2-alloc-unsafe.rs

---
+++ data/Box::from_raw/2-alloc-unsafe.rs
@@ -0,0 +1,13 @@
+
+#![allow(unused)]
+fn main() {
+use std::alloc::{alloc, Layout};
+
+fn main() {
+    let ptr = unsafe { 
+        alloc(Layout::new::<i32>()) as *mut i32
+    };
+    unsafe { ptr.write(0); }
+    let mut x = unsafe { Box::from_raw(ptr) };
+    *x = 2;
+}
