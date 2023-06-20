1bf4f040ab74db2255f54b0071cdfeb7b17a0453
XU, Hui <china.xuhui@gmail.com>
2023-06-20 09:04:09 +0800

    Update 4-misused-void-unsafe-1.rs

--- data_test/transmute/4-misused-void-unsafe-1.rs
+++ data_test/transmute/4-misused-void-unsafe-1.rs
@@ -1,3 +1,6 @@
+/*
+From: https://github.com/dbcfd/suricata/blob/95729e923f39b5adda31e4843a1b6ee06edbb0c8/rust/src/applayertemplate/template.rs#L287
+*/
 pub extern "C" fn rs_template_state_new(_orig_state: *mut std::os::raw::c_void, _orig_proto: AppProto) -> *mut std::os::raw::c_void {
     let state = TemplateState::new();
     let boxed = Box::new(state);
