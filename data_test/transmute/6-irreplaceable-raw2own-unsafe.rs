d81710c601492b691697d13d0c308dd8a89e44ad
DDDdyan <90452649+DDDdyan@users.noreply.github.com>
2023-06-20 09:33:21 +0800

    Create 6-irreplaceable-raw2own-unsafe.rs

---
+++ data_test/transmute/6-irreplaceable-raw2own-unsafe.rs
@@ -0,0 +1,12 @@
+
+# https://github.com/esp-rs/rust/blob/4896f6b455b005c410fb7ef87a2f96bf5b1b73cb/library/std/src/sys_common/net.rs#L158
+
+unsafe {
+                let cur = self.cur.as_ref()?;
+                self.cur = cur.ai_next;
+                match sockaddr_to_addr(mem::transmute(cur.ai_addr), cur.ai_addrlen as usize) {
+                    Ok(addr) => return Some(addr),
+                    Err(_) => continue,
+                }
+}
+
