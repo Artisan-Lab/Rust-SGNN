ff4a32b3b7414d2dc056037488e52b6d0e2009fb
DDDdyan <90452649+DDDdyan@users.noreply.github.com>
2023-06-20 09:34:06 +0800

    Update 6-irreplaceable-raw2own-unsafe.rs

--- data_test/transmute/6-irreplaceable-raw2own-unsafe.rs
+++ data_test/transmute/6-irreplaceable-raw2own-unsafe.rs
@@ -1,5 +1,6 @@
-
-# https://github.com/esp-rs/rust/blob/4896f6b455b005c410fb7ef87a2f96bf5b1b73cb/library/std/src/sys_common/net.rs#L158
+/*
+https://github.com/esp-rs/rust/blob/4896f6b455b005c410fb7ef87a2f96bf5b1b73cb/library/std/src/sys_common/net.rs#L158
+*/
 
 unsafe {
                 let cur = self.cur.as_ref()?;
@@ -9,4 +10,3 @@ unsafe {
                     Err(_) => continue,
                 }
 }
-
