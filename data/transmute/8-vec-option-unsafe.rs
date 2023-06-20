e9134459e43914100f4d3b7b239fd5694336bf38
XU, Hui <china.xuhui@gmail.com>
2023-06-20 16:22:25 +0800

    Update 8-vec-option-unsafe.rs

--- data/transmute/8-vec-option-unsafe.rs
+++ data/transmute/8-vec-option-unsafe.rs
@@ -4,9 +4,8 @@ From: https://doc.rust-lang.org/std/mem/fn.transmute.html
 #![allow(unused)]
 fn main() {
     let store = [0, 1, 2, 3];
-    let v_orig = store.iter().collect::<Vec<&i32>>();
-    let v_clone = v_orig.clone();
-    let v_transmuted = unsafe {
-        std::mem::transmute::<Vec<&i32>, Vec<Option<&i32>>>(v_clone)
+    let v = store.iter().collect::<Vec<&i32>>();
+    let v = unsafe {
+        std::mem::transmute::<Vec<&i32>, Vec<Option<&i32>>>(v)
     };
 }
