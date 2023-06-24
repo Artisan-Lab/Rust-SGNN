024233e2fcd98a42c12643ab00b75866d171b341
DDDdyan <90452649+DDDdyan@users.noreply.github.com>
2023-06-24 22:44:40 +0800

    Update 1-simple-unsafe.rs

--- data/Box::from_raw_in/1-simple-unsafe.rs
+++ data/Box::from_raw_in/1-simple-unsafe.rs
@@ -7,7 +7,7 @@ fn foo() -> Result<(), impl core::fmt::Debug> {
         System.allocate(Layout::new::<i32>())?.as_mut_ptr() as *mut i32
     };
     unsafe { ptr.write(0); }
-    let mut x = unsafe { Box::from_raw(ptr) };
+    let mut x = unsafe { Box::from_raw_in(ptr) };
     *x = 2;
     Ok::<(), std::alloc::AllocError>(())
 }
